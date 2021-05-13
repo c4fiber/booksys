dd4a1253828696be0dbf9a78a671a6c25ecaa8c8
c4fiber <qudcjf153@gmail.com>
2021-05-13 15:08:32 +0900

    session 없는 로그인 구현
    차후 session part 추가하여 세션 유지될 수 있도록 할 예정입니다.

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -6,9 +6,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +33,12 @@ public class MainController {
 		Database.getInstance();
 	}
 
+	// 기본페이지. 테스트용입니다.
+	@RequestMapping("/")
+	public String index(HttpSession session) {
+		return "index";
+	}
+	
 	@RequestMapping("/timeTable")
 	public String timeTable() {
 		return "timeTable";
@@ -39,17 +49,42 @@ public class MainController {
 		return "login";
 	}
 	
+	// 회원가입 페이지 로딩
 	@RequestMapping("/register")
 	public String register() {
 		return "register";
 	}
 	
+	// 회원가입 로직 처리
 	@RequestMapping("/register.do")
-	public String register_do() {
+	public String register_do(@RequestParam("id") String id, 
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("phoneNumber") String phoneNumber) {
 		
+		String result = "done";
+		try {
+			Statement stmt
+			  = Database.getConnection().createStatement() ;
+			int updateCount = stmt.executeUpdate(
+					"INSERT INTO user (id, password, name, phoneNumber)" +
+					       "VALUES ('" + id + "', '" + password + "', '" + name + "', '" + phoneNumber + "')");
+			stmt.close() ;
+		}catch (SQLException e) {
+			e.printStackTrace() ;
+			result = "fail";
+		}
 		
-		return "timeTable";
-	}
+		//TODO 
+		/* 정상적으로 작동하는지 확인하기 위해 return 값을 string 고정하였다. 차후 수정필요함.
+		 * 아래는 예시 코드
+		 * http://localhost:8080/register.do?id=test&password=1234&name=bb&phoneNumber=01012341234
+		 */
+		return "index";
+	}	
+	
+	// 
+
 	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
@@ -61,25 +96,24 @@ public class MainController {
 		return String.format("TESTING NOW!!!! =  %s", name);
 	}
 
-	@GetMapping("/error")
-	public String error() {
-		return String.format("ERROR OCCURED!!!!");
-	}
 
-	@GetMapping("/login.do")
-	public boolean login(String id, String password, String name, String phoneNumber) {
+
+	@RequestMapping("/login.do")
+	public String login(HttpServletRequest request, @RequestParam("id") String id, @RequestParam("password") String password, Model model) {
 		// 1. id키가 기본키이므로 id키로 Select문을 사용해 User객체를 받아옴
 		User findUser = getUser("SELECT * FROM user WHERE id = '" + id + "'");
 		// 2. 없으면 false리턴
 		if (findUser == null)
-			return false;
+			return "index";
 		else {
 			// 3.id와 패스워드 같으면
 			if (id.equals(findUser.getId()) && password.equals(findUser.getPassword())) {
 				// 트루
-				return true;
-			} else
-				return false;
+				
+				HttpSession session = request.getSession();
+				session.setAttribute("name", findUser.getName());
+				model.addAttribute("id", id);
+			}
 		}
 		/*
 		 * 이줄은 필수가 아닙니다. 읽지 않으셔도 됩니다. /AND password = '" + password + "'"
@@ -88,6 +122,9 @@ public class MainController {
 		 * &&name.equals(findUser.getName())&&phoneNumber.equals(findUser.getPhoneNumber
 		 * ()) 매개변수와 User객체 비교하는데 쓸 수 있는 보조문 (필수아님)
 		 */
+			
+	
+		return "index";
 	}
 
 	/*
@@ -101,10 +138,10 @@ public class MainController {
 			ResultSet rset = stmt.executeQuery(sql);
 			while (rset.next()) {
 				int oid = rset.getInt(1);
-				String id = rset.getString(2);
-				String password = rset.getString(3);
-				String name = rset.getString(4);
-				String phoneNumber = rset.getString(5);
+				String id = rset.getString(3);
+				String password = rset.getString(4);
+				String name = rset.getString(5);
+				String phoneNumber = rset.getString(6);
 				c = new User(oid, id, password, name, phoneNumber);
 			}
 			rset.close();
@@ -165,32 +202,7 @@ public class MainController {
 		return result;
 	}	
 	
-	@GetMapping("/register.do")
-	public String register(@RequestParam(value = "id") String id, 
-			@RequestParam(value = "password") String password,
-			@RequestParam(value = "name") String name,
-			@RequestParam(value = "phoneNumber") String phoneNumber) {
-		
-		String result = "done";
-		try {
-			Statement stmt
-			  = Database.getConnection().createStatement() ;
-			int updateCount = stmt.executeUpdate(
-					"INSERT INTO user (id, password, name, phoneNumber)" +
-					       "VALUES ('" + id + "', '" + password + "', '" + name + "', '" + phoneNumber + "')");
-			stmt.close() ;
-		}catch (SQLException e) {
-			e.printStackTrace() ;
-			result = "fail";
-		}
-		
-		//TODO 
-		/* 정상적으로 작동하는지 확인하기 위해 return 값을 string 고정하였다. 차후 수정필요함.
-		 * 아래는 예시 코드
-		 * http://localhost:8080/register.do?id=test&password=1234&name=bb&phoneNumber=01012341234
-		 */
-		return result;
-	}	
+
 }
 
 
