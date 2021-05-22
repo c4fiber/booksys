636bda8ce1b494ddaf9eb5426961f82f3d40553c
c4fiber <qudcjf153@gmail.com>
2021-05-22 14:43:45 +0900

    main controller 수정 / 예약 및 테이블 jsp form action 수정
    메인 컨트롤러에서 예약이 되었는지 안되었는지 확인하는 코드 수정
    reservation.jsp / timeTable.jsp 의 form action 타겟을 수정

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -34,7 +34,7 @@ public class MainController {
 		SpringApplication.run(MainController.class, args);
 		Database.getInstance();
 	}
-	
+
 	// 기본 페이지
 	@RequestMapping("/")
 	public String index(HttpSession session) {
@@ -70,14 +70,14 @@ public class MainController {
 		 * 01012341234
 		 */
 		return "index";
-	}	
-	
+	}
+
 	// 로그인
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@RequestMapping("/login.do")
 	public String login(HttpServletRequest request, @RequestParam("id") String id,
 			@RequestParam("password") String password, Model model) {
@@ -106,14 +106,15 @@ public class MainController {
 
 		return "index";
 	}
-	
+
 	// 타임테이블 / 예약
 	@RequestMapping("/timeTable")
 	public String timeTable() {
+
 		return "timeTable";
 	}
-	
-	//TODO DAO 객체 만들어서 처리
+
+	// TODO DAO 객체 만들어서 처리
 	/*
 	 * //column oid id password name phoneNumber => user 0 1 2 3 각 User의 column 내용을
 	 * 데이터베이스에서 가져와서 User 객체를 돌려준다.
@@ -139,14 +140,31 @@ public class MainController {
 		return c;
 	}
 
+	/* 예약 파트 */
+	@RequestMapping("/reservation")
+	public String reservation() {
+		return "reservation";
+	}
+
+	@RequestMapping("/checkReservation")
+	public String reservation(@RequestParam int tableNumber, @RequestParam float time, Model model) {
+		// 날짜, 시간 기준으로 예약내역 조회
+		try {
+			Statement stmt = Database.getConnection().createStatement();
+			ResultSet checkSet = stmt.executeQuery("SELECT oid from reservation where");
+		} catch (Exception e) {
+
+		}
+
+		return "reservation";
+	}
+
+	// 예약하기
 	@RequestMapping("/reservation.do")
-	public String reservation(
-			@RequestParam(value = "customer_id") int customer_id,
-			@RequestParam(value = "table_id") String table_id,
-			@RequestParam(value = "time") Time time,
-			@RequestParam(value = "date") String date,
-			@RequestParam(value = "covers") int covers,
-			@RequestParam(value = "oid") int oid){
+	public String reservation(@RequestParam(value = "customer_id") int customer_id,
+			@RequestParam(value = "table_id") String table_id, @RequestParam(value = "time") Time time,
+			@RequestParam(value = "date") String date, @RequestParam(value = "covers") int covers,
+			@RequestParam(value = "oid") int oid, Model model) {
 		String result = "done";
 		try {
 			Statement stmt = Database.getConnection().createStatement();
@@ -156,7 +174,8 @@ public class MainController {
 					+ "AND time=" + "'" + time + "'" + "AND table_id=" + "'" + table_id + "'");
 			while (checkSet.next()) {
 				if (checkSet.getInt(1) > 0) {
-					return "이미 배정된 테이블";
+					model.addAttribute("status", 0);
+					return "timeTable";
 				}
 			}
 			checkSet.close();
@@ -175,14 +194,16 @@ public class MainController {
 		/*
 		 * public Reservation(int c, Date d, Time t, Table tab, Customer cust, Time arr)
 		 * arrivalTime은 아직 없으므로 NULL 고려해야할 점 1.table_id 받아 왔을 때 이미 예약 내에 같은 table_id가
-		 * 있으면 거부해 주는 작업 필요할 것 예상 (Select문) // 초안 구현 완료 2.user_id로 결정 4. PM님 시도대로 똑같은 코드
-		 * 제작 //TODO /* 정상적으로 작동하는지 확인하기 위해 return 값을 string 고정하였다. 차후 수정필요함. 아래는 예시 코드
+		 * 있으면 거부해 주는 작업 필요할 것 예상 (Select문) // 초안 구현 완료 2.user_id로 결정 4. PM님 시도대로 똑같은 코드 제작
+		 * 
+		 * //TODO Reservation 처리 /* 정상적으로 작동하는지 확인하기 위해 return 값을 string 고정하였다. 차후
+		 * 수정필요함. 아래는 예시 코드
 		 * http://localhost:8080/reservation.do?customer_id=1234&table_id=01&time=10:00:
 		 * 00&date=1971-01-21&covers=5&oid=12
 		 * 
 		 * customer -> 후에 user_id로 변경될 예정
 		 */
-		return result;
+		return "timeTable";
 	}
 
 	/*
