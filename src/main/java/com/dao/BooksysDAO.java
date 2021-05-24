15e8cae1074420ded2a34594c84ab0922d933c47
gkapffms45 <82950982+gkapffms45@users.noreply.github.com>
2021-05-24 19:48:17 +0900

    Update BooksysDAO.java
    review 기능 구현

--- src/main/java/com/dao/BooksysDAO.java
+++ src/main/java/com/dao/BooksysDAO.java
@@ -15,11 +15,12 @@ import com.domain.User;
 
 @Repository
 public class BooksysDAO {
-	@Autowired JdbcTemplate jt;
+	@Autowired
+	JdbcTemplate jt;
 
 	// table 전체 불러오기, dbTableSelect.jsp와 연동
 	public List<Map<String, ?>> selectAll() {
-		
+
 		return jt.query("select * from `table`", (rs, rowNum) -> {
 			Map<String, Object> mss = new HashMap<>();
 			mss.put("oid", rs.getInt(1));
@@ -28,30 +29,29 @@ public class BooksysDAO {
 			return mss;
 		});
 	}
-	
+
 	// 로그인: match하는 User Object(id, name 갱신) 반환. 없으면 null
-	public User login(String id, String password){
-		
+	public User login(String id, String password) {
+
 		// queryForObject를 사용하면 결과가 0개일때 exception 발생
-		return DataAccessUtils.singleResult(jt.query("select id, name, password FROM user where id = ? and password = ?", 
-				(rs, rowNum) -> new User(rs.getString("id"),rs.getString("name")),
-					id, password)
-				);
+		return DataAccessUtils
+				.singleResult(jt.query("select id, name, password FROM user where id = ? and password = ?",
+						(rs, rowNum) -> new User(rs.getString("id"), rs.getString("name")), id, password));
 	}
-	
+
 	// 회원가입 데이터 삽입: return 1(성공), 0(실패)
 	public int register(String id, String password, String name, String phoneNumber) {
 		// insert문은 update method를 사용한다.
 		String sql = "INSERT INTO user (id, password, name, phoneNumber) VALUES (?,?,?,?)";
 		int result = jt.update(sql, id, password, name, phoneNumber);
-		
+
 		return result;
 	}
 	//
-	
-	//date인자를 받아서 입력될 날짜에 해당하는 예약 리스트로 뽑아올수있게
-	public List<Map<String,?>> takeAllReservationsUseDate(Date date){
-		return jt.query("select * from reservation where date='"+date.toString()+"'", (rs,rowNum)->{
+
+	// date인자를 받아서 입력될 날짜에 해당하는 예약 리스트로 뽑아올수있게
+	public List<Map<String, ?>> takeAllReservationsUseDate(Date date) {
+		return jt.query("select * from reservation where date='" + date.toString() + "'", (rs, rowNum) -> {
 			Map<String, Object> mss = new HashMap<>();
 			mss.put("covers", rs.getInt(2));
 			mss.put("date", rs.getDate(3));
@@ -62,12 +62,11 @@ public class BooksysDAO {
 			return mss;
 		});
 	}
-	
-	//mss->리뷰를 가져올께요
-	
+
+
 	// 예약 조회
 	public List<Map<String, ?>> selectAllReservations() {
-		
+
 		return jt.query("select * from reservation", (rs, rowNum) -> {
 			Map<String, Object> mss = new HashMap<>();
 			mss.put("covers", rs.getInt(2));
@@ -76,37 +75,50 @@ public class BooksysDAO {
 			mss.put("table_id", rs.getInt(5));
 			mss.put("customer_id", rs.getInt(6));
 			mss.put("arrivalTime", rs.getTime(7));
-			
+
 			return mss;
 		});
 	}
 	
-	//해당 날짜 해당 테이블에 중복된 예약이 있는지 확인
-	public boolean nowTableReservationAvailable(Date date,Time time,int table_id)
-	{
-		jt.query("SELECT oid from reservation WHERE date=" + "'" + date + "'"
-					+ "AND time=" + "'" + time + "'" + "AND table_id=" + "'" + table_id + "'",(rs,rowNum)->{
-						Map<String, Object> mss = new HashMap<>();
-						mss.put("customer_id", rs.getInt(6));
-						if(mss.isEmpty()) 
-							return true;
-						return false;
-						});
+	// 리뷰 조회
+	public List<Map<String, ?>> selectAllComments() {
+
+		return jt.query("select * from comment", (rs, rowNum) -> {
+			Map<String, Object> mss = new HashMap<>();
+			mss.put("id", rs.getInt(2));
+			mss.put("date", rs.getDate(3));
+			mss.put("commnet", rs.getString(4));
+
+			return mss;
+		});
+	}
+
+	// 해당 날짜 해당 테이블에 중복된 예약이 있는지 확인
+	public boolean nowTableReservationAvailable(Date date, Time time, int table_id) {
+		jt.query("SELECT oid from reservation WHERE date=" + "'" + date + "'" + "AND time=" + "'" + time + "'"
+				+ "AND table_id=" + "'" + table_id + "'", (rs, rowNum) -> {
+					Map<String, Object> mss = new HashMap<>();
+					mss.put("customer_id", rs.getInt(6));
+					if (mss.isEmpty())
+						return true;
+					return false;
+				});
 		return false;
 	}
-	
+
 	// 예약 추가
-		public void addReservation(int covers,Date date,Time time,int table_id,int customer_id) {
-				String sql ="INSERT INTO reservation ( customer_id, table_id, time,date,covers)" + "VALUES ("+ customer_id
-						+ "," + table_id + ",'" + time + "', '" + date + "',"  + covers+")";
-				jt.update(sql);
-		}
-	
-	// 리뷰 작성 
+	public void addReservation(int covers, Date date, Time time, int table_id, int customer_id) {
+		String sql = "INSERT INTO reservation ( customer_id, table_id, time,date,covers)" + "VALUES (" + customer_id
+				+ "," + table_id + ",'" + time + "', '" + date + "'," + covers + ")";
+		jt.update(sql);
+	}
+
+	// 리뷰 작성
 	public int addReview(String id, String comment, String date) {
 		String sql = "INSERT INTO review (id, date, comment) VALUES (?,?,?)";
 		int result = jt.update(sql, id, date, comment);
-		
+
 		return result;
 	}
+
 }
