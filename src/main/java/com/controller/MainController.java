0584af5fae930e301a3868bc9878f192664cffb2
c4fiber <qudcjf153@gmail.com>
2021-05-31 14:31:09 +0900

    table_number, startTime, endTime을 상대적으로 적용
    table_number는 db에서 가져와서 사용
    startTime, endTime은 controller 최상단에서 적용

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -52,13 +52,26 @@ public class MainController {
 	List<Map<String, ?>> cacheR = new ArrayList<Map<String, ?>>();
 
 	/*
-	 *  session확보용 객체
+	 * session확보용 객체
 	 */
 	User user = new User("", "anonymous");
 
+	/**
+	 * 식당 운영시간 (default = 16 ~ 24)
+	 */
+	final int startTime = 16;
+	final int endTime = 24;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainController.class, args);
-		Database.getInstance();
+	}
+
+	final static Date today() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String dateStr = dtf.format(LocalDateTime.now());
+		Date date = Date.valueOf(dateStr);
+
+		return date;
 	}
 
 	/*
@@ -81,6 +94,7 @@ public class MainController {
 
 	/**
 	 * 회원가입
+	 * 
 	 * @param id
 	 * @param password
 	 * @param name
@@ -111,9 +125,9 @@ public class MainController {
 		return "redirect:/";
 	}
 
-	
 	/**
 	 * 로그인 체크
+	 * 
 	 * @param id
 	 * @param password
 	 * @param model
@@ -123,6 +137,7 @@ public class MainController {
 	public String login(@RequestParam(value = "id", defaultValue = "") String id,
 			@RequestParam(value = "password", defaultValue = "") String password, Model model) {
 
+		System.out.println("id&password" + id + password);
 		User result = booksysDAO.login(id, password);
 		if (result != null) {
 			user.setId(result.getId());
@@ -130,7 +145,7 @@ public class MainController {
 
 			return "redirect:/";
 		} else {
-			model.addAttribute("alert", "yes");
+			model.addAttribute("failed", "yes");
 
 			return "/login";
 		}
@@ -140,9 +155,22 @@ public class MainController {
 	 * 타임 테이블 페이지
 	 */
 	@RequestMapping("/timeTable")
-	public String timeTable(Model model) {
+	public String timeTable(@RequestParam(value = "date", required = false) Date date, Model model) {
+		// 입력 날짜가 없으면 오늘날짜로 입력(처음 timetable 열람 시)
+		if (date == null) {
+			System.out.println("debug: ok");
+			date = today();
+		}
 		model.addAttribute("id", user.getId());
 		System.out.println(user.getId());
+
+		// table 개수, 입력 날짜, 영업 시작시간, 영업 종료시간
+		int numOfTables = booksysDAO.selectNumOfTables();
+		model.addAttribute("numOfTables", numOfTables);
+		model.addAttribute("date", date.toString());
+		model.addAttribute("startTime", this.startTime);
+		model.addAttribute("endTime", this.endTime);
+
 		return "timeTable";
 	}
 
@@ -154,57 +182,72 @@ public class MainController {
 		return "reservation";
 	}
 
-	
 	/**
-	 * 모든 예약을 불러온다.
-	 * @param tableNumber
+	 * 예약 추가
+	 * 
+	 * @param customer_id
+	 * @param table_id
 	 * @param time
-	 * @param model
+	 * @param date
+	 * @param covers
 	 * @return
 	 */
-	@RequestMapping("/AllReservations.do")
-	public String reservation(@RequestParam int tableNumber, @RequestParam float time, Model model) {
-		model.addAttribute("reservations", booksysDAO.selectAllReservations());
+	@RequestMapping("/reservation.do")
+	public synchronized String reservation(@RequestParam(value = "customer_id") int customer_id,
+			@RequestParam(value = "table_id") int table_id, @RequestParam(value = "time") Time time,
+			@RequestParam(value = "date") Date date, @RequestParam(value = "covers") int covers) {
+		boolean bookAvailable = booksysDAO.nowTableReservationAvailable(date, time, table_id);
+		if (bookAvailable) {
+			booksysDAO.addReservation(covers, date, time, table_id, customer_id);
+		}
 
-		return "reservation";
+		return "redirect:/timetable";
+
 	}
 
-	
-
 	/**
-	 * 예약 가능한지 불가능한지 테이블에 따라 판단해주는 메소드 
+	 * 예약 가능한지 불가능한지 체크
+	 * 
 	 * @param model
 	 * @param date
 	 * @return
 	 */
-	@RequestMapping(value="/check.do",method = RequestMethod.POST)
-	public synchronized String check(Model model,Date date)
-	{ 
-		int tables = 5; // 테이블 개수
-		int partTimes = 4; // 운영파트 개수 (운영시간: 16 ~ 24 -> 8시간 -> 4개 파트  
-		int startTime = 16; // 식당 시작시간 (default = 16)
-		model.addAttribute("id", user.getId());
-		model.addAttribute("superDate",date);
-		//((int)((i*100)+16+j)값의 경우 예를 들어 2번테이블 18시일경우 218이 key가 됩니다.
-		// 시간은  00~ 18 20 22 24~이고 테이블은 무한정 늘어날 수 있기 때문에 로직을 그러하게 작성하였습니다.
-		for (int i = 1; i <= tables; i++) {
-				for (float j = 0; j < partTimes*2; j=j+2) {
-					Time time =new Time((int) (startTime+j), 0, 0);					
-					model.addAttribute((int)((i*100)+startTime+j)+"", booksysDAO.nowTableReservationAvailable(date, time, i)+"");
-				}
+	@RequestMapping(value = "/check.do", method = RequestMethod.POST)
+	public synchronized String check(Model model, Date date) {
+		int tableNum = booksysDAO.selectNumOfTables(); // 전체 테이블 개수
+
+		model.addAttribute("id", user.getId()); // 로그인한 user_id
+		model.addAttribute("superDate", date); // 예약을 조회할 날짜
+
+		// ((int)((i*100)+16+j)값의 경우 예를 들어 2번테이블 18시일경우 218이 key가 됩니다.
+		// 시간은 00~ 18 20 22 24~이고 테이블은 무한정 늘어날 수 있기 때문에 로직을 그러하게 작성하였습니다.
+		for (int i = 1; i <= tableNum; i++) {
+			for (int j = startTime; j < endTime; j = j + 2) {
+				Time time = new Time((int) (startTime + j), 0, 0);
+				model.addAttribute((i * 100 + j) + "", booksysDAO.nowTableReservationAvailable(date, time, i) + "");
+			}
 		}
-		return "timeTable";	
+
+		// List형태로 예약상황 조회 다루기
+		List<String> rStatus = booksysDAO.reservationStatus(date);
+		for (String bean : rStatus) {
+			System.out.println(bean); // TODO string이여야 하는데 int로 받아짐. 수정필요
+		}
+		System.out.println(rStatus);
+
+		return "timeTable";
 	}
-	
+
 	/**
 	 * 리뷰 페이지 로딩
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping("/review")
 	public String review(Model model) {
 		final int COUNT = 10; // 페이지 당 리뷰 개수
-		
+
 		System.out.println(booksysDAO.selectAllReviews());
 		model.addAttribute("reviews", booksysDAO.selectAllReviews());
 		model.addAttribute("user_id", user.getId());
@@ -214,6 +257,7 @@ public class MainController {
 
 	/**
 	 * review 추가. comment를 DB로 넘겨줍니다.
+	 * 
 	 * @param user_id
 	 * @param comment
 	 * @param model
@@ -236,92 +280,84 @@ public class MainController {
 				model.addAttribute("result", "sql fail");
 			}
 		}
-		
+
 		return "redirect:/review";
 	}
-	
+
 	/**
 	 * table 모두 출력
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping("/select.do")
 	public String selectAllTable(Model model) {
 		model.addAttribute("results", booksysDAO.selectAllTables());
-		model.addAttribute("reservations", booksysDAO.selectAllReservations());
 
 		return "dbTableSelect";
 	}
-	
-	
-	
+
 	/**
 	 * 예약 추가
+	 * 
 	 * @param table_id
 	 * @param time
 	 * @param date
 	 * @param covers
-	 * @return DB에 입력 
+	 * @return DB에 입력
 	 */
-		@RequestMapping("/reservation.do")
-		public String reservation1(
-				@RequestParam(value = "table_id") String[] table_id, @RequestParam(value = "time") String[] time,
-				@RequestParam(value = "date") String[] date, @RequestParam(value = "covers") String[] covers,Model model,@RequestParam(value = "id") String[] id) throws SQLException {
-			
-			String resultMessage = null;
-			
-			List<Map<String,Integer>> tempUserList = booksysDAO.findUserOiduseUser_id(id[0]);
-			Map<String,Integer> tempMap = tempUserList.get(0);
-			int user_oid = tempMap.get("oid");
-			
+	@RequestMapping("/reservation.do")
+	public String reservation1(@RequestParam(value = "table_id") String[] table_id,
+			@RequestParam(value = "time") String[] time, @RequestParam(value = "date") String[] date,
+			@RequestParam(value = "covers") String[] covers, Model model, @RequestParam(value = "id") String[] id)
+			throws SQLException {
+
+		String resultMessage = null;
+
+		List<Map<String, Integer>> tempUserList = booksysDAO.findUserOiduseUser_id(id[0]);
+		Map<String, Integer> tempMap = tempUserList.get(0);
+		int user_oid = tempMap.get("oid");
+
+		/*
+		 * 타임테이블에 입력한 모든값 넣기
+		 */
+		for (int i = 0; i < date.length; i++) {
 			/*
-			 * 타임테이블에 입력한 모든값 넣기
-			 * */
-			for(int i=0;i<date.length;i++)
-			{
-				/*
-				 * 값이 빈거 체크
-				 * */
-				if(covers[i].equals("")||time[i].equals("")||date[i].equals("")||table_id[i].equals(""))
-				{
-					resultMessage = resultMessage+i+"번째 줄 입력값에 빈칸이 있는 항목은 적용되지 않았습니다.\n";
-					continue;
-				}
-				Time tempTime = Time.valueOf(time[i]);
-				Date tempDate = Date.valueOf(date[i]);
-				int tempCovers = -500;
-				int temptable_id = -500;
-				/*
-				 * 굳이 없어도 되기는 하는데 숫자값 입력 체크
-				 * */
-				try {
-					tempCovers = Integer.valueOf(covers[i]);
-					temptable_id = Integer.valueOf(table_id[i]);
-				}
-				catch(NumberFormatException e)
-				{
-					resultMessage = resultMessage+i+"번째 줄 테이블 입력값이 잘못 되었습니다.\n";
-				}
-				/*
-				 * 성공과 실패 구분
-				 * */
-				int success = booksysDAO.addReservation(tempCovers, tempDate, tempTime, temptable_id, user_oid);
-				if(success==1)
-				{
-					resultMessage += date[i]+"일자" +time[i]+"분"+table_id[i]+"번 테이블 예약되었습니다.\n";
-				}
-				else
-				{
-					resultMessage += date[i]+"일자" +time[i]+"분"+table_id[i]+"번 테이블 예약하였습니다.\n";
-				}
-			}
-			
-			/*
-			 * resultPage로 메시지 넘기고 종료
+			 * 값이 빈거 체크
 			 */
-			model.addAttribute("Message", resultMessage);
-			return "reservationResult";
+			if (covers[i].equals("") || time[i].equals("") || date[i].equals("") || table_id[i].equals("")) {
+				resultMessage = resultMessage + i + "번째 줄 입력값에 빈칸이 있는 항목은 적용되지 않았습니다.\n";
+				continue;
+			}
+			Time tempTime = Time.valueOf(time[i]);
+			Date tempDate = Date.valueOf(date[i]);
+			int tempCovers = -500;
+			int temptable_id = -500;
+			/*
+			 * 굳이 없어도 되기는 하는데 숫자값 입력 체크
+			 */
+			try {
+				tempCovers = Integer.valueOf(covers[i]);
+				temptable_id = Integer.valueOf(table_id[i]);
+			} catch (NumberFormatException e) {
+				resultMessage = resultMessage + i + "번째 줄 테이블 입력값이 잘못 되었습니다.\n";
+			}
+			/*
+			 * 성공과 실패 구분
+			 */
+			int success = booksysDAO.addReservation(tempCovers, tempDate, tempTime, temptable_id, user_oid);
+			if (success == 1) {
+				resultMessage += date[i] + "일자" + time[i] + "분" + table_id[i] + "번 테이블 예약되었습니다.\n";
+			} else {
+				resultMessage += date[i] + "일자" + time[i] + "분" + table_id[i] + "번 테이블 예약하였습니다.\n";
+			}
 		}
-	
+
+		/*
+		 * resultPage로 메시지 넘기고 종료
+		 */
+		model.addAttribute("Message", resultMessage);
+		return "reservationResult";
+	}
 
 }
