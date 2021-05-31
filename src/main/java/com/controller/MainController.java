d28fa32c0ce7d255338483965f23cf998441fc56
00100010101110110111100110 <werber112@kyonggi.ac.kr>
2021-05-31 14:48:57 +0900

    reservation 오류 수정 (#28)
    * appropriate 구현완료
    
    mainController 수정
    mainController return값 변경
    register
    timeTable반환할 수 있게
    
    TableMapper
    appropriateTable 구현 완료
    cover는 `table`에서 추가될 경우 추가 해서 구현하겠습니다
    
    register.jsp 변경 완료
    PM님 말씀대로 바꿨던 것입니다.
    
    * Update MainController.java
    
    * appropriateTable
    
    appropriateTable
    인원수에 따른 자동 배정 반영
    
    * 오류 수정
    
    개인적으로 해보는 곳에 import 되어 수정
    
    * comment 구현
    
    domain
    Comment 객체 추가
    
    getComment(게시판 목차 1,2,3)
    디비에서 데이터 읽어서 Comment객체가진 Vector반환(일단 10개씩)
    
    putComment
    Comment를 한개 db로 삽입
    
    * index 페이지 세션 로그인 로그아웃구현
    
    로그인 => 로그아웃 => 로그인기능
    
    * jsp변경
    
    jsp변경
    
    * 2가지 기능 구현 완료
    
    reservation구현 완료
    날짜에 맞는 Map가져오는 것 구현 완료
    
    * Update BooksysDAO.java
    
    * selectAllcomments
    
    다가져오기
    
    * 테이블 예약 가능여부 판단
    
    1. timetable을 변경하였습니다.
    기존 파일은 OrigintimeTable.jsp에 있습니다.
    
    2. BooksysDAO에 예약이 가능한지 불가능한지 판단하는 메소드인 nowTableReservationAvailable 메소드를 수정했습니다.
    
    3.MainController에
    check.do가 추가 되었습니다.
    check.do는 timeTable을 요청하면 자동으로 실행되는 메소드입니다.
    
    * temp
    
    temp
    
    * Update timeTable.jsp
    
    * 중간작업
    
    수정중
    
    * Update BooksysDAO.java
    
    * addreservation 구현
    
    구현함
    
    * Update MainController.java
    
    * 오류 잡기
    
    1. 이제 check.do가 URL에 보이지 않습니다.
    2. 입력 폼 없을 때는 등록 버튼 막았습니다.
    3. 로그인 없이 예약할 경우 잘못된 접근 페이지로 보냅니다.
    
    Co-authored-by: c4fiber <qudcjf153@gmail.com>

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -154,15 +154,17 @@ public class MainController {
 	/*
 	 * 타임 테이블 페이지
 	 */
-	@RequestMapping("/timeTable")
+
 	public String timeTable(@RequestParam(value = "date", required = false) Date date, Model model) {
-		// 입력 날짜가 없으면 오늘날짜로 입력(처음 timetable 열람 시)
+		model.addAttribute("id", user.getId());
+		System.out.println(user.getId());
+  
+    // 입력 날짜가 없으면 오늘날짜로 입력(처음 timetable 열람 시)
 		if (date == null) {
 			System.out.println("debug: ok");
 			date = today();
 		}
-		model.addAttribute("id", user.getId());
-		System.out.println(user.getId());
+
 
 		// table 개수, 입력 날짜, 영업 시작시간, 영업 종료시간
 		int numOfTables = booksysDAO.selectNumOfTables();
@@ -170,7 +172,7 @@ public class MainController {
 		model.addAttribute("date", date.toString());
 		model.addAttribute("startTime", this.startTime);
 		model.addAttribute("endTime", this.endTime);
-
+    
 		return "timeTable";
 	}
 
@@ -182,14 +184,16 @@ public class MainController {
 		return "reservation";
 	}
 
-	/**
-	 * 예약 추가
+	@RequestMapping(value="/check.do",method = RequestMethod.GET)
+	public synchronized String goHome()
+	{ 
+		return "index";	
+
+    	/**
+	 * 예약 가능한지 불가능한지 체크
 	 * 
-	 * @param customer_id
-	 * @param table_id
-	 * @param time
+	 * @param model
 	 * @param date
-	 * @param covers
 	 * @return
 	 */
 	@RequestMapping("/reservation.do")
@@ -202,25 +206,18 @@ public class MainController {
 		}
 
 		return "redirect:/timetable";
-
 	}
 
-	/**
-	 * 예약 가능한지 불가능한지 체크
-	 * 
-	 * @param model
-	 * @param date
-	 * @return
-	 */
-	@RequestMapping(value = "/check.do", method = RequestMethod.POST)
-	public synchronized String check(Model model, Date date) {
+	@RequestMapping(value="/check.do",method = RequestMethod.POST)
+	public synchronized String check(Model model,Date date)
+	{ 
 		int tableNum = booksysDAO.selectNumOfTables(); // 전체 테이블 개수
-
-		model.addAttribute("id", user.getId()); // 로그인한 user_id
-		model.addAttribute("superDate", date); // 예약을 조회할 날짜
-
-		// ((int)((i*100)+16+j)값의 경우 예를 들어 2번테이블 18시일경우 218이 key가 됩니다.
-		// 시간은 00~ 18 20 22 24~이고 테이블은 무한정 늘어날 수 있기 때문에 로직을 그러하게 작성하였습니다.
+  
+		model.addAttribute("id", user.getId());
+		model.addAttribute("superDate",date);
+  
+		//((int)((i*100)+16+j)값의 경우 예를 들어 2번테이블 18시일경우 218이 key가 됩니다.
+		// 시간은  00~ 18 20 22 24~이고 테이블은 무한정 늘어날 수 있기 때문에 로직을 그러하게 작성하였습니다.
 		for (int i = 1; i <= tableNum; i++) {
 			for (int j = startTime; j < endTime; j = j + 2) {
 				Time time = new Time((int) (startTime + j), 0, 0);
@@ -232,7 +229,7 @@ public class MainController {
 		List<String> rStatus = booksysDAO.reservationStatus(date);
 		for (String bean : rStatus) {
 			System.out.println(bean); // TODO string이여야 하는데 int로 받아짐. 수정필요
-		}
+
 		System.out.println(rStatus);
 
 		return "timeTable";
@@ -296,6 +293,85 @@ public class MainController {
 
 		return "dbTableSelect";
 	}
+	
+	
+	
+	/**
+	 * 예약 추가
+	 * @param table_id
+	 * @param time
+	 * @param date
+	 * @param covers
+	 * @return DB에 입력 
+	 */
+		@RequestMapping("/reservation.do")
+		public String reservation1(
+				@RequestParam(value = "table_id") String[] table_id, @RequestParam(value = "time") String[] time,
+				@RequestParam(value = "date") String[] date, @RequestParam(value = "covers") String[] covers,Model model,@RequestParam(value = "id") String[] id) throws SQLException {
+			int user_oid=-500;
+			String resultMessage = "";
+			try {
+				List<Map<String,Integer>> tempUserList = booksysDAO.findUserOiduseUser_id(id[0]);
+				Map<String,Integer> tempMap = tempUserList.get(0);
+				user_oid = tempMap.get("oid");
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				resultMessage = "잘못된 접근입니다.";
+				model.addAttribute("Message", resultMessage);
+				return "reservationResult";
+			}
+			
+			
+			/*
+			 * 타임테이블에 입력한 모든값 넣기
+			 * */
+			for(int i=0;i<date.length;i++)
+			{
+				/*
+				 * 값이 빈거 체크
+				 * */
+				if(covers[i].equals("")||time[i].equals("")||date[i].equals("")||table_id[i].equals(""))
+				{
+					resultMessage = resultMessage+(i+1)+"번째 줄 입력값에 빈칸이 있는 항목은 적용되지 않았습니다.<br>";
+					continue;
+				}
+				Time tempTime = Time.valueOf(time[i]);
+				Date tempDate = Date.valueOf(date[i]);
+				int tempCovers = -500;
+				int temptable_id = -500;
+				/*
+				 * 굳이 없어도 되기는 하는데 숫자값 입력 체크
+				 * */
+				try {
+					tempCovers = Integer.valueOf(covers[i]);
+					temptable_id = Integer.valueOf(table_id[i]);
+				}
+				catch(NumberFormatException e)
+				{
+					resultMessage = resultMessage+(i+1)+"번째 줄 테이블 입력값이 잘못 되었습니다.<br>";
+				}
+				/*
+				 * 성공과 실패 구분
+				 * */
+				int success = booksysDAO.addReservation(tempCovers, tempDate, tempTime, temptable_id, user_oid);
+				if(success==1)
+				{
+					resultMessage += date[i]+"일자 " +time[i]+"분 "+table_id[i]+"번 테이블 예약되었습니다.<br>";
+				}
+				else
+				{
+					resultMessage += date[i]+"일자 " +time[i]+"분 "+table_id[i]+" 번 테이블 예약하였습니다.<br>";
+				}
+			}
+			
+			/*
+			 * resultPage로 메시지 넘기고 종료
+			 */
+			model.addAttribute("Message", resultMessage);
+			return "reservationResult";
+		}
+	
 
 	/**
 	 * 예약 추가
